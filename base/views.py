c2a7693ee0d4da549368c29b5b2812c09a9ec660
Rakib <naym.mj@gmail.com>
2024-09-10 23:11:17 +0600

    create data

--- base/views.py
+++ base/views.py
@@ -2,8 +2,16 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
+rooms = [
+    {'id':1,'name':'Lets learn python!'},
+    {'id':2,'name':'Design with me!'},
+    {'id':3,'name':'Frontend developers'},
+    {'id':4,'name':'AI Engine!!!'},
+]
+
 def home(request):
-    return render(request,'home.html')
+    context = {"Data":rooms}
+    return render(request,'home.html',context)
 
 def room(request):
     return render(request,'room.html')
